e3b3d462837ab9cb7f9864d4471908ef677dc114
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-10-13 18:08:48 +0200

    fix(sitemap): fix some parsing errors

---
+++ views/default/seo/sitemap/urlset.php
@@ -0,0 +1,22 @@
+<?php
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+	<?php
+	$urls = elgg_extract('urls', $vars, []);
+	foreach ($urls as $url) {
+		?>
+		<url>
+			<?php
+			foreach ($url as $tag => $value) {
+				if (empty($value)) {
+					continue;
+				}
+				echo elgg_format_element($tag, [], htmlentities($value, ENT_QUOTES, 'UTF-8')) . PHP_EOL;
+			}
+			?>
+		</url>
+		<?php
+	}
+	?>
+</urlset>
