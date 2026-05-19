925feda75ae4b2fc9f1c11f6256c3da3d6393c57
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-05-19 18:12:39 +0200

    fix(migrate/elgg-7.x): get_user_by_username() removed in 5.x → elgg_get_user_by_username()

--- classes/hypeJunction/Seo/RewriteService.php
+++ classes/hypeJunction/Seo/RewriteService.php
@@ -101,6 +101,10 @@ class RewriteService {
 	 * @return string|false
 	 */
 	public function normalizeUri($url = '') {
+		// elgg_normalize_url() requires string in 7.x (was lenient in 3.x).
+		if (!is_string($url) || $url === '') {
+			return false;
+		}
 		$url = elgg_normalize_url($url);
 		$site_url = elgg_get_site_url();
 		if (strpos($url, $site_url) !== 0) {
